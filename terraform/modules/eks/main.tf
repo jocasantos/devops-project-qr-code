558112728f507c365975139abfd68cfe98f1e6e3
João Santos <joaopcsantos1@gmail.com>
2025-02-11 16:15:53 +0000

    refactor: terraform eks modeule

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -25,17 +25,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
-resource "aws_kms_key" "example" {
-  description = "KMS key for EKS cluster"
-  is_enabled  = true
-}
-
-resource "aws_kms_alias" "this" {
-  name          = "alias/eks/${var.cluster_name}"
-  target_key_id = aws_kms_key.example.key_id
-}
-
-resource "aws_cloudwatch_log_group" "this" {
-  name = "/aws/eks/${var.cluster_name}/cluster"
-}

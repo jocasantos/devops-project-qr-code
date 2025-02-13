f1214728358e64ee7a193153e2245677bdf00a68
João Santos <joaopcsantos1@gmail.com>
2025-02-13 13:55:19 +0000

    Refactor: main.py API

--- api/main.py
+++ api/main.py
@@ -24,10 +24,7 @@ app.add_middleware(
 )
 
 # AWS S3 Configuration
-s3 = boto3.client(
-    's3',
-    aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
+s3 = boto3.client('s3')
 
 bucket_name = os.getenv("S3_BUCKET")
 
